120334a138e9f5b0769c7619b8633b9978352bf4
Loading <3547323964@qq.com>
2025-03-31 17:01:35 +0800

    增加了闪避值 智力 暴伤 护甲统计数据

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -1,13 +1,20 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strength;
-    public Stat damage;
+    [Header("Major stats")]
+    public Stat strength; // 1 点表示提升暴击伤害1%
+    public Stat agility;  // 1 点表示闪避提高1%
+    public Stat intelligence; // 1 点表示提高魔法伤害2 点
+    public Stat vitality; // 1 点能提高生命上限1 点
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+    public Stat damage;
 
     public int currentHealth;
 
@@ -19,10 +26,15 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targeStat)
     {
+        if (TargetCanAvoidAttack(_targeStat))
+            return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
 
+        totalDamage = CheckTargetArmor(_targeStat, totalDamage);
         _targeStat.TakeDamage(totalDamage);
     }
+
 
     public virtual void TakeDamage(int _damage)
     {
@@ -35,5 +47,23 @@ public class CharacterStats : MonoBehaviour
     protected virtual void Die()
     {
 
+    }
+
+    private int CheckTargetArmor(CharacterStats _targeStat, int totalDamage)
+    {
+        totalDamage -= _targeStat.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targeStat)
+    {
+        int totalEvasion = _targeStat.evasion.GetValue() + _targeStat.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+        }
+        return false;
     }
 }
